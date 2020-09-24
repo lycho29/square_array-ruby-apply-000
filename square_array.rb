3125104410990a66d17585b0f479a652022fce63
Lydia Cho <lydiacho@gse.havard.edu>
2020-09-24 15:58:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,5 @@
-#def square_array(array)
- # arr = []
-  #array.each { |i| arr << i ** 2 }
-  #arr
-#end
-
-def square_array(arr)
-  numbers = [1, 2, 3]
-  arr.each {|num| numbers << num ** 2 }
-  numbers
+def square_array(array)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
